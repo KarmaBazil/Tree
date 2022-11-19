c62901d87c359d35c08076de4b8994d59c28cc83
Home <karmarae85@gmail.com>
2022-11-18 20:59:17 -0500

    add src folder

---
+++ src/Tree.java
@@ -0,0 +1,18 @@
+
+public class Tree {
+    public static void main(String[] args) {
+        //This program will introduce me and print a tree to the screen.
+        System.out.println("Hi everyone, my name is Karma and I am learning how to code with Java. So far I love it!");
+        System.out.println("Today, I am going to put up my christmas tree!");
+        System.out.println("     *     ");
+        System.out.println("    ***    ");
+        System.out.println("   *****   ");
+        System.out.println("  *******  ");
+        System.out.println(" ********* ");
+        System.out.println("***********");
+        System.out.println("     **    ");
+
+
+
+    }
+}
